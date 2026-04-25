50d2e2b86bc0baaae25ed5426154f14dd8acbcb2
juan david <juandavidmancilla653@gmail.com>
2026-04-25 13:42:38 -0500

    Agregar servicio InscripcionCurso para gestionar inscripciones de cursos

--- practica-mvc/src/main/java/com/universidad/servicios/InscripcionCursoService.java
+++ practica-mvc/src/main/java/com/universidad/servicios/InscripcionCursoService.java
@@ -1,5 +1,13 @@
 package com.universidad.servicios;
 
+import com.universidad.dao.InscripcionCursoDao;
+import com.universidad.model.InscripcionCurso;
+
 public class InscripcionCursoService {
 
+    private InscripcionCursoDao dao = new InscripcionCursoDao();
+
+    public void crear(InscripcionCurso inscripcion) {
+        dao.guardar(inscripcion);
+    }
 }
