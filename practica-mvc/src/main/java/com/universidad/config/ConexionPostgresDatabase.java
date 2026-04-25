87b1d619a2d40355119cb3ff46565084590f8a8e
juan david <juandavidmancilla653@gmail.com>
2026-04-25 16:46:26 -0500

    Actualizar la URL de conexión a la base de datos PostgreSQL en la clase de configuración

--- practica-mvc/src/main/java/com/universidad/config/ConexionPostgresDatabase.java
+++ practica-mvc/src/main/java/com/universidad/config/ConexionPostgresDatabase.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class ConexionPostgresDatabase {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/universidad";
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USER = "postgres";
     private static final String PASSWORD = "elunico123";
 
